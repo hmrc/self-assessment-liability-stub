8ab732350429be776c033570092c54451d55949c
aaocs <132440545+aaocs@users.noreply.github.com>
2025-06-03 16:18:34 +0100

    add implementation and tests for citizen details call

---
+++ test/controllers/CitizenDetailsControllerSpec.scala
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import play.api.http.Status
+import play.api.libs.json.Json
+import play.api.test.Helpers._
+import play.api.test.{FakeRequest, Helpers}
+
+class CitizenDetailsControllerSpec extends AnyWordSpec with Matchers {
+
+  private val fakeRequest = FakeRequest("GET", "/")
+  private val controller = new CitizenDetailsController(Helpers.stubControllerComponents())
+
+  "GET /" should {
+    "return 200 with correct NINO for a valid UTR" in {
+      val result = controller.getNino("any other UTR")(fakeRequest)
+      status(result) shouldBe Status.OK
+      contentAsJson(result) shouldBe Json.parse(controller.validSuccessResponse)
+    }
+
+    "return 400 BAD_REQUEST with correct error message for invalid UTR" in {
+      val result = controller.getNino("EE999400E")(fakeRequest)
+      status(result) shouldBe Status.BAD_REQUEST
+      contentAsJson(result) shouldBe Json.obj(
+        "message" -> "Invalid SaUtr."
+      )
+    }
+
+    "return 404 NOT_FOUND with correct error message for no matching UTR" in {
+      val result = controller.getNino("EE999404E")(fakeRequest)
+      status(result) shouldBe Status.NOT_FOUND
+      contentAsJson(result) shouldBe Json.obj(
+        "message" -> "No record for the given SaUtr is found."
+      )
+    }
+
+    "return 500 INTERNAL_SERVER_ERROR with correct error message for multiple matching UTRs" in {
+      val result = controller.getNino("EE999500E")(fakeRequest)
+      status(result) shouldBe Status.INTERNAL_SERVER_ERROR
+      contentAsJson(result) shouldBe Json.obj(
+        "message" -> "More than one valid matching result."
+      )
+    }
+
+    "return 500 INTERNAL_SERVER_ERROR with correct error message when service unavailable" in {
+      val result = controller.getNino("EE999500F")(fakeRequest)
+      status(result) shouldBe Status.INTERNAL_SERVER_ERROR
+      contentAsJson(result) shouldBe Json.obj(
+        "message" -> "Service currently unavailable"
+      )
+    }
+  }
+}
