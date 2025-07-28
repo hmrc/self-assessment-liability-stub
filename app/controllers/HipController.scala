b8c35fe9a2c685598f2b17e27f390e5ead1cdaca
alikouhestani <90757950+alikouhestani@users.noreply.github.com>
2025-07-28 15:28:33 +0100

    generate dynamic data for HIP based on year

--- app/controllers/HipController.scala
+++ app/controllers/HipController.scala
@@ -16,6 +16,7 @@
 
 package controllers
 
+import models.{AccruingInterestDateRange, Amendments, BalanceDetails, ChargeDetails, CodedOutDetail, HipResponse, PaymentHistoryDetails, RefundDetails}
 import utils.constants.RequestResponseConstants.*
 import utils.date.DateParser.StringParser
 import play.api.libs.json.Json
@@ -23,9 +24,12 @@ import play.api.mvc.{Action, AnyContent, ControllerComponents}
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 
 import java.text.{ParseException, SimpleDateFormat}
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import java.util.Date
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.Future
+import scala.util.Random
 
 @Singleton()
 class HipController @Inject() (cc: ControllerComponents) extends BackendController(cc) {
@@ -85,3 +89,170 @@ class HipController @Inject() (cc: ControllerComponents) extends BackendControll
       }
   }
 }
+
+object HipController {
+
+    private val random = new Random()
+    private val chargeTypes = List("ITSA", "Penalty", "PAYE")
+    private val amendmentTypes = List("payment", "credit", "adjustment")
+    private val paymentMethods = List("bank transfer", "card", "direct debit", "cheque")
+    private val refundStatuses = List("processed", "pending", "rejected")
+
+
+    def generateDocumentsFromYear(fromDate: LocalDate): Map[Int, HipResponse] = {
+      val startYear = fromDate.getYear
+      val currentYear = LocalDate.now().getYear
+
+      (startYear to currentYear).map { year =>
+        year -> generateDocumentForYear(year)
+      }.toMap
+    }
+  
+    private def generateDocumentForYear(year: Int): HipResponse = {
+      val numCharges = random.nextInt(2) + 1
+      val charges = (1 to numCharges).map(_ => generateCharge(year)).toSet
+
+      val balanceDetails = generateBalanceDetails(year, charges)
+      val refunds = generateRefunds(year)
+      val paymentHistory = generatePaymentHistory(year, charges)
+
+      HipResponse(balanceDetails, Some(charges), Some(refunds), Some(paymentHistory))
+    }
+
+    private def generateBalanceDetails(year: Int, charges: Set[ChargeDetails]): BalanceDetails = {
+      val totalOutstanding = charges.map(_.outstandingAmount).sum
+      val totalChargeAmount = charges.map(_.chargeAmount).sum
+      val totalCodedOut = charges.flatMap(_.codedOutDetail.getOrElse(Set.empty)).flatMap(_.amount).sum
+
+      BalanceDetails(
+        totalOverdueBalance = if (year < LocalDate.now().getYear) totalOutstanding else 0.00,
+        totalPayableBalance = totalOutstanding * random.nextDouble(),
+        payableDueDate = generateDateInYear(year, isEndOfYear = true),
+        totalPendingBalance = totalOutstanding + random.nextInt(2000),
+        pendingDueDate = generateFutureDate(year),
+        totalBalance = totalChargeAmount,
+        totalCodedOut = totalCodedOut,
+        totalCreditAvailable = random.nextInt(1000)
+      )
+    }
+
+    private def generateCharge(year: Int): ChargeDetails = {
+      val chargeAmount = random.nextInt(5000) + 500
+      val outstandingAmount = chargeAmount * random.nextDouble()
+      val chargeType = chargeTypes(random.nextInt(chargeTypes.length))
+
+      val amendments = if (random.nextBoolean()) {
+        (1 to random.nextInt(3) + 1).map(_ => generateAmendment(year, chargeAmount)).toSet
+      } else Set.empty
+
+      val codedOut = if (random.nextBoolean() && year < LocalDate.now().getYear) {
+        Set(generateCodedOutDetail(year, chargeType))
+      } else Set.empty
+      val interestStartDate = generateDateInYear(year + 1)
+      val interestEndDate = generateDateInYear(year + 1, isEndOfYear = true)
+      val isLate = random.nextBoolean()
+      val interestAmount =  Some(random.nextInt(200).toDouble)
+      ChargeDetails(
+        chargeId = generateChargeId(),
+        creationDate = generateDateInYear(year),
+        chargeType = chargeType,
+        chargeAmount = chargeAmount,
+        outstandingAmount = outstandingAmount,
+        taxYear = s"${year}-${year + 1}",
+        dueDate = generateDateInYear(year + 1),
+        interestAmountDue = if isLate then interestAmount else None, 
+        accruingInterest = if isLate then interestAmount else None, 
+        accruingInterestDateRange = if isLate then Some(AccruingInterestDateRange(interestStartDate,interestEndDate)) else None,
+        accruingInterestRate = if isLate then Some(0.05) else None,
+        amendments = Some(amendments),
+        codedOutDetail = Some(codedOut)
+      )
+    }
+
+    private def generateAmendment(year: Int, maxAmount: Double): Amendments = {
+    val amendmentReason = amendmentTypes(random.nextInt(amendmentTypes.length))
+    val amendmentAmount = maxAmount * random.nextDouble()
+
+    Amendments(
+      amendmentDate = generateDateInYear(year),
+      amendmentAmount = amendmentAmount,
+      amendmentReason = amendmentReason,
+      newChargeBalance = Some(maxAmount - amendmentAmount),
+      paymentMethod = if (amendmentReason == "payment") Some(paymentMethods(random.nextInt(paymentMethods.length))) else None,
+      paymentDate = if (amendmentReason == "payment") Some(generateDateInYear(year)) else None
+    )
+  }
+
+    private def generateCodedOutDetail(year: Int, chargeType: String): CodedOutDetail = {
+      CodedOutDetail(
+        amount = Some(random.nextInt(500) + 100),
+        effectiveDate = Some(generateDateInYear(year)),
+        taxYear = Some(s"${year}-${year + 1}"),
+        effectiveTaxYear = Some(s"${year + 1}-${year + 2}")
+      )
+    }
+
+    private def generateRefunds(year: Int): Set[RefundDetails] = {
+      if (random.nextBoolean() && year < LocalDate.now().getYear) {
+        val numRefunds = random.nextInt(2) + 1
+        (1 to numRefunds).map(_ => {
+          val requestAmount = random.nextInt(1000) + 100
+          val interest = requestAmount * 0.015
+
+          RefundDetails(
+            issueDate = generateDateInYear(year),
+            refundMethod = Some(paymentMethods(random.nextInt(paymentMethods.length))),
+            refundRequestDate = Some(generateDateInYear(year - 1, isEndOfYear = true)),
+            refundRequestAmount = requestAmount,
+            refundReference = Some(generatePaymentReference()),
+            interestAddedToRefund = Some(interest),
+            refundActualAmount = requestAmount + interest,
+            refundStatus = Some(refundStatuses(random.nextInt(refundStatuses.length)))
+          )
+        }).toSet
+      } else Set.empty
+    }
+
+    private def generatePaymentHistory(year: Int, charges: Set[ChargeDetails]): Set[PaymentHistoryDetails] = {
+    charges.flatMap { charge =>
+      charge.amendments.getOrElse(Set.empty)
+        .filter(_.amendmentReason == "payment")
+        .map { amendment =>
+          PaymentHistoryDetails(
+            paymentAmount = amendment.amendmentAmount,
+            paymentId = generatePaymentReference(),
+            paymentMethod = amendment.paymentMethod.getOrElse("bank_transfer"),
+            paymentDate = amendment.paymentDate.getOrElse(generateDateInYear(year)),
+            dateProcessed = amendment.amendmentDate,
+            allocationReference = Some(charge.chargeId)
+          )
+        }
+    }
+  } 
+  
+    private def generatePaymentReference(): String = random.nextInt(1231232131).toString
+
+    private def generateChargeId(): String = {
+      val letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+      val prefix = (1 to 2).map(_ => letters(random.nextInt(letters.length))).mkString
+      val numbers = (1 to 7).map(_ => random.nextInt(10)).mkString
+      s"$prefix$numbers"
+    }
+  
+    private def generateDateInYear(year: Int, isEndOfYear: Boolean = false): String = {
+      val month = if (isEndOfYear) random.nextInt(6) + 7 else random.nextInt(12) + 1 
+      val maxDay = month match {
+        case 2 => if (year % 4 == 0) 29 else 28
+        case 4 | 6 | 9 | 11 => 30
+        case _ => 31
+      }
+      val day = random.nextInt(maxDay) + 1
+      LocalDate.of(year, month, day).format(DateTimeFormatter.ISO_LOCAL_DATE)
+    }
+
+    private def generateFutureDate(baseYear: Int): String = {
+      val futureYear = baseYear + random.nextInt(2) + 1
+      generateDateInYear(futureYear)
+    }
+  }
+
